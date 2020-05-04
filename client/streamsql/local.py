64d859a72974ab3fae31c2297d3fc28c23e05b41
Simba <simba@streamsql.io>
2020-05-04 14:34:11 -0700

    Add base of local.FeatureStore
    The python local.FeatureStore can read a CSV and turn it into a table.
    The local table can performed indexed look ups via a primary key. The
    local.FeatureStore will grow into a version of StreamSQL that can be
    used locally, though it will likely have a subset of the core
    functionality since it doesn't make much sense to have Kafka and other
    streaming primitives locally.
    
    The local.Table is a thin wrapper over a pandas dataframe to allow us
    to use the pandasql package to mimic core feature store functionality
    locally.

---
+++ client/streamsql/local.py
@@ -0,0 +1,42 @@
+from streamsql.errors import TableAlreadyExists
+import pandas
+
+
+class FeatureStore:
+    """Feature is a local in-memory feature store
+
+    The local feature store provides a subset of the core functionality
+    of the backend one. It should only be used for models that are
+    trained and used locally.
+    """
+    def __init__(self):
+        self._tables = dict()
+
+    def create_table_from_csv(self, csv_file, table_name="", primary_key=""):
+        """Create a table from a local csv file"""
+        if table_name in self._tables:
+            raise TableAlreadyExists(table_name)
+
+        table = Table(csv_file=csv_file, primary_key=primary_key)
+        self._tables[table_name] = table
+        return table
+
+
+class Table:
+    """Table is an in-memory implementation of the StreamSQL table"""
+    def __init__(self, csv_file="", primary_key=""):
+        """Table can be instatiated from a CSV file."""
+        self._dataframe = self._dataframe_from_csv(csv_file, primary_key)
+
+    def lookup(self, key):
+        """Lookup returns an array from a table by its primary key"""
+        item_series = self._dataframe.loc[key]
+        return item_series.to_list()
+
+    def _dataframe_from_csv(self, file_name, index_col):
+        dataframe = pandas.read_csv(file_name, index_col=index_col)
+        self._clean_dataframe(dataframe)
+        return dataframe
+
+    def _clean_dataframe(self, dataframe):
+        dataframe.index = dataframe.index.astype(str, copy=False)
