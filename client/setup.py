4c634ce6f3470710654f55633a1ece24dde00c40
Simba <simba@streamsql.io>
2020-05-04 11:20:33 -0700

    Initializes Python client
    - Adds basic Python .gigignore
    - Sets up base of Makefile for Python client
    - Adds setup.py for Python client

---
+++ client/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+from setuptools import find_packages, setup
+
+with open('requirements.txt', 'r') as req_file:
+    requirements = [req.strip() for req in req_file.read().splitlines()]
+
+with open("../README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="streamsql",
+    version="0.0.0a1",
+    url="https://github.com/streamsql-io/streamsql",
+    description="Python SDK for the StreamSQL feature store",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="The StreamSQL Team",
+    author_email="simba@streamsql.io",
+    install_requires=requirements,
+    python_requires=">=3.6.0",
+    packages=setuptools.find_packages(exclude=("tests", )),
+    license="AGPLv3",
+    classifiers=[
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Development Status :: 3 - Alpha"
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+    ],
+)
