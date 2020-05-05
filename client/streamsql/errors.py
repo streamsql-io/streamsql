22d9831231bd88969fd37792411f2af88b1e19d0
Simba <simba@streamsql.io>
2020-05-05 14:17:43 -0700

    Rename TableAlreadyExists to TableExistsError
    nit: More Pythonic error name since it ends in Error.

--- client/streamsql/errors.py
+++ client/streamsql/errors.py
@@ -2,7 +2,7 @@ class Error(Exception):
     """Base class for public StreamSQL client errors."""
 
 
-class TableAlreadyExists(Error):
+class TableExistsError(Error):
     """Raised when the requested table name already exists."""
     def __init__(self, name):
         self.message = "Table with name \"{0}\" already exists".format(name)
