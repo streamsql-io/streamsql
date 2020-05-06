1207428077698bf71e65bbac925a7144faa55793
Simba <simba@streamsql.io>
2020-05-06 12:31:24 -0700

    Add MinMax Operation
    MinMax will be used as a scaling operation for Feature.

--- client/streamsql/operation.py
+++ client/streamsql/operation.py
@@ -11,6 +11,30 @@ class NoOp:
         return value
 
 
+class MinMax:
+    """MinMax linearly scales a feature to fit between a min and max"""
+    def __init__(self, min=0, max=1):
+        self._min = min
+        self._max = max
+        self._range = max - min
+
+    def name(self, name):
+        return "min_max"
+
+    def apply(self, column, value):
+        clm_max = column.max()
+        clm_min = column.min()
+        clm_range = clm_max - clm_min
+        # Transform value to what it would be if the column was scaled from zero
+        # to one.
+        zero_to_one = (value - clm_min) / clm_range
+        # Transform value to what it would be if the column was scaled from zero
+        # to this MinMax range.
+        scaled = zero_to_one * self._range
+        # Shift value so that it starts at the minimum.
+        return scaled + self._min
+
+
 class Sqrt:
     @classmethod
     def name(cls):
