5886002a15cd078a528eac30b60899925f9cebe1
Simba <simba@streamsql.io>
2020-06-22 16:23:22 -0700

    Set Python client version to 0.0.1
    It's cleaner to not have any characters in the version.

--- client/setup.py
+++ client/setup.py
@@ -10,7 +10,7 @@ with open("../README.md") as f:
 
 setuptools.setup(
     name="streamsql",
-    version="0.0.0a1",
+    version="0.0.1",
     url="https://github.com/streamsql-io/streamsql",
     description="Python SDK for the StreamSQL feature store",
     long_description=long_description,
