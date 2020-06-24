a2ea323068ddeefe0e6c0b084f82bb09ff6da800
Simba <simba@streamsql.io>
2020-06-24 10:51:31 -0700

    Add _clm to all column tests in test_table
    We are also adding table tests to that file, this makes the distinction
    clearer.

--- client/tests/test_table.py
+++ client/tests/test_table.py
@@ -10,18 +10,18 @@ def count100clm():
     return Column("count100", pandas.Series(range(1, 101)))
 
 
-def test_getitem(count100clm):
+def test_clm_getitem(count100clm):
     assert count100clm[50] == 51
 
 
-def test_transform_rename(count100clm):
+def test_clm_transform_rename(count100clm):
     identity = lambda x: x
     abc = count100clm.transform(identity, name="abc")
     assert count100clm.name() == "count100"
     assert abc.name() == "abc"
 
 
-def test_transform_pass_column(count100clm):
+def test_clm_transform_pass_column(count100clm):
     shift_fn = lambda x, column: x - column.max()
     max_shifted = count100clm.transform(shift_fn, pass_column=True)
     expected = list(range(-99, 1))
@@ -29,39 +29,39 @@ def test_transform_pass_column(count100clm):
         assert max_shifted[i] == expected[i]
 
 
-def test_transform(count100clm):
+def test_clm_transform(count100clm):
     doubled = count100clm.transform(lambda x: x * 2)
     assert doubled[0] == 2
     assert count100clm[0] == 1
 
 
-def test_transform_sideeffects(count100clm):
+def test_clm_transform_sideeffects(count100clm):
     doubled = count100clm.transform(lambda x: x * 2)
     assert doubled[0] == 2
     assert count100clm[0] == 1
 
 
-def test_mean(count100clm):
+def test_clm_mean(count100clm):
     assert count100clm.mean() == 50.5
 
 
-def test_median(count100clm):
+def test_clm_median(count100clm):
     assert count100clm.mean() == 50.5
 
 
-def test_std(count100clm):
+def test_clm_std(count100clm):
     np.testing.assert_almost_equal(count100clm.std(), 29.0114, 4)
 
 
-def test_max(count100clm):
+def test_clm_max(count100clm):
     assert count100clm.max() == 100
 
 
-def test_min(count100clm):
+def test_clm_min(count100clm):
     assert count100clm.min() == 1
 
 
-def test_quantile(count100clm):
+def test_clm_quantile(count100clm):
     actual = count100clm.quantile(0, .25, .5, .75, 1)
     expected = [1, 25.75, 50.5, 75.25, 100]
     assert (actual == expected).all()
