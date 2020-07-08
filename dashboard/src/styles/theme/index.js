dc7283493b50b1436b0592204f17e6e91a45b5d5
Simba <simba@streamsql.io>
2020-07-08 10:16:54 -0700

    Adds formatting to dashboard project
    Uses prettier to format all types of files in the dashboard project.
    It can be run using 'npx prettier --write' for now, it has not yet been
    included in the CI or the Makefile.

---
+++ dashboard/src/styles/theme/index.js
@@ -0,0 +1,20 @@
+import blueGrey from "@material-ui/core/colors/blueGrey";
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+
+const GREEN = "#27ae60";
+const PURPLE = "#8e44ad";
+
+const themeSpec = {
+  palette: {
+    primary: { main: GREEN, contrastText: "#FFFFFF" },
+    secondary: { main: PURPLE },
+    text: {
+      primary: blueGrey[900],
+      secondary: blueGrey[700],
+    },
+  },
+};
+
+let theme = createMuiTheme(themeSpec);
+theme = responsiveFontSizes(theme);
+export default theme;
