2ce8a510eed9b0caa77e88999a54baa209e81144
Simba <simba@streamsql.io>
2020-07-17 11:34:54 -0700

    Move Material-UI wrapper into App.js from index.js
    This gets more logic out of index.js

--- dashboard/src/components/app/App.js
+++ dashboard/src/components/app/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "styles/theme";
+import { ThemeProvider } from "@material-ui/core/styles";
 import Nav from "components/nav";
 
-function App() {
-  return <Nav></Nav>;
-}
+export const App = (props) => (
+  <Wrapper><Nav></Nav></Wrapper>
+);
+
+export const Wrapper = ({ children }) => (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      { children }
+    </ThemeProvider>
+);
 
 export default App;
--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import theme from "styles/theme";
-import { ThemeProvider } from "@material-ui/core/styles";
 import "styles/base.css";
 import App from "./components/app";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <App />
     ,
   </React.StrictMode>,
   document.querySelector("#root")
