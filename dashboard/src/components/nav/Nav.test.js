12297ef97b43ec897ba0c092eead76c65b302b6e
Simba <simba@streamsql.io>
2020-07-17 09:49:02 -0700

    Add DrawerListLink Unit Tests
    These were not added in a past commit with the same tagline.
    
    It tests both internal and external links.

---
+++ dashboard/src/components/nav/Nav.test.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import renderer from "react-test-renderer";
+
+import { DrawerListLink } from "./Nav.js";
+
+configure({ adapter: new Adapter() });
+
+describe("Nav", () => {
+  const classes = {
+    navlink_active: "",
+    appBar: "",
+  };
+
+  describe("DrawerListLink", () => {
+    // These tests are run on both internal and external NavLinks
+    const genericTests = (val) => {
+      it("renders children", () => {
+        expect(val.children()).toHaveLength(1);
+        expect(val.first().text()).toEqual("abc");
+      });
+    };
+
+    describe("internal", () => {
+      const internal = shallow(
+        <DrawerListLink path="/abc" classes={classes}>
+          <div>abc</div>
+        </DrawerListLink>
+      );
+      genericTests(internal);
+      it("Sets path correctly", () => {
+        expect(internal.props().to).toEqual("/abc");
+      });
+    });
+
+    describe("external", () => {
+      const external = shallow(
+        <DrawerListLink
+          path="https://streamsql.io"
+          classes={classes}
+          external={true}
+        >
+          <div>abc</div>
+        </DrawerListLink>
+      );
+      genericTests(external);
+      it("Sets href correctly", () => {
+        expect(external.props().href).toEqual("https://streamsql.io");
+      });
+    });
+  });
+});
