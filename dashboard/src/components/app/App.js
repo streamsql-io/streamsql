517c97a3f356b58c76aaf6488927cbf13efc24e4
Simba <simba@streamsql.io>
2020-07-20 21:17:03 -0700

    Move section definitions out of Nav
    Rather than hard-coding the sections in Nav, we inject them in from
    App component.
    
    We have to also hard-code the current version into the Nav test to
    pass the snapshot test. We could change the snapshot, but I think
    this is a better test.

--- dashboard/src/components/app/App.js
+++ dashboard/src/components/app/App.js
@@ -4,9 +4,44 @@ import theme from "styles/theme";
 import { ThemeProvider } from "@material-ui/core/styles";
 import Nav from "components/nav";
 
+const sections = [
+  {
+    name: "Resources",
+    items: [
+      { text: "Data Sources", icon: "file-import", path: "/sources" },
+      { text: "Materialized Views", icon: "copy", path: "/views" },
+      { text: "Features", icon: "file-code", path: "/features" },
+      { text: "Feature Sets", icon: "sitemap", path: "/feature-sets" },
+      { text: "Training Sets", icon: "archive", path: "/training-sets" },
+    ],
+  },
+  {
+    name: "Monitoring",
+    items: [
+      { text: "Metrics", icon: "chart-line", path: "/metrics" },
+      { text: "Deployment", icon: "server", path: "/deployment" },
+    ],
+  },
+  {
+    name: "Admin",
+    items: [
+      { text: "Users", icon: "users", path: "/users" },
+      { text: "Settings", icon: "cogs", path: "/settings" },
+      { text: "Billing", icon: "wallet", path: "/billing" },
+      {
+        text: "Documentation",
+        icon: "book",
+        path: "https://docs.streamsql.io",
+        external: true,
+      },
+      { text: "Help", icon: "question", path: "/help" },
+    ],
+  },
+];
+
 export const App = (props) => (
   <Wrapper>
-    <Nav></Nav>
+    <Nav sections={sections}></Nav>
   </Wrapper>
 );
 
--- dashboard/src/components/nav/Nav.test.js
+++ dashboard/src/components/nav/Nav.test.js
@@ -14,9 +14,44 @@ describe("Nav", () => {
   };
 
   it("hasn't changed", () => {
+    const example_sections = [
+      {
+        name: "Resources",
+        items: [
+          { text: "Data Sources", icon: "file-import", path: "/sources" },
+          { text: "Materialized Views", icon: "copy", path: "/views" },
+          { text: "Features", icon: "file-code", path: "/features" },
+          { text: "Feature Sets", icon: "sitemap", path: "/feature-sets" },
+          { text: "Training Sets", icon: "archive", path: "/training-sets" },
+        ],
+      },
+      {
+        name: "Monitoring",
+        items: [
+          { text: "Metrics", icon: "chart-line", path: "/metrics" },
+          { text: "Deployment", icon: "server", path: "/deployment" },
+        ],
+      },
+      {
+        name: "Admin",
+        items: [
+          { text: "Users", icon: "users", path: "/users" },
+          { text: "Settings", icon: "cogs", path: "/settings" },
+          { text: "Billing", icon: "wallet", path: "/billing" },
+          {
+            text: "Documentation",
+            icon: "book",
+            path: "https://docs.streamsql.io",
+            external: true,
+          },
+          { text: "Help", icon: "question", path: "/help" },
+        ],
+      },
+    ];
+
     const tree = renderer
       .create(
-        <Nav>
+        <Nav sections={example_sections}>
           <div>abc</div>
           <div>def</div>
         </Nav>
