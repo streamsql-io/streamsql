771dca54d56b542f33ee82fc6e706ae83e74cf61
Simba <simba@streamsql.io>
2020-07-20 23:50:07 -0700

    Add initial Sources component
    Sources is a material table that will eventually show a list of
    resources that can be expanded and interacted with.

---
+++ dashboard/src/components/sources/Sources.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import MaterialTable from "material-table";
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  table: {
+    margin: theme.spacing(4),
+  },
+}));
+
+export const Sources = ({ title }) => {
+  const classes = useStyles();
+  return (
+    // MaterialTable doesn't support className, so we wrap it in a Box:
+    // https://github.com/mbrn/material-table/issues/766
+    <Box className={classes.table}>
+      <MaterialTable
+        title={title}
+        columns={[{ title: "Name", field: "name" }]}
+        data={[{ name: "test1" }, { name: "test2" }]}
+        options={{
+          search: true,
+        }}
+      />
+    </Box>
+  );
+};
+
+export default Sources;
