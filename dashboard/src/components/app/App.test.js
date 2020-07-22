68803423a729ad8420a80a3a5df2d48a56a30f1c
Simba <simba@streamsql.io>
2020-07-22 08:40:48 -0700

    Move NavSections definition into Redux
    In the future, there may be logic to change the Nav sidebar based on
    the context (local, hosted, managed) and if the user is paying or not.

--- dashboard/src/components/app/App.test.js
+++ dashboard/src/components/app/App.test.js
@@ -9,8 +9,43 @@ import { App, indexPath, parseContentProps, Wrapper } from "./App.js";
 configure({ adapter: new Adapter() });
 
 describe("App", () => {
+  const example_sections = [
+    {
+      name: "Resources",
+      items: [
+        { title: "Data Sources", icon: "file-import", path: "/sources" },
+        { title: "Materialized Views", icon: "copy", path: "/views" },
+        { title: "Features", icon: "file-code", path: "/features" },
+        { title: "Feature Sets", icon: "sitemap", path: "/feature-sets" },
+        { title: "Training Sets", icon: "archive", path: "/training-sets" },
+      ],
+    },
+    {
+      name: "Monitoring",
+      items: [
+        { title: "Metrics", icon: "chart-line", path: "/metrics" },
+        { title: "Deployment", icon: "server", path: "/deployment" },
+      ],
+    },
+    {
+      name: "Admin",
+      items: [
+        { title: "Users", icon: "users", path: "/users" },
+        { title: "Settings", icon: "cogs", path: "/settings" },
+        { title: "Billing", icon: "wallet", path: "/billing" },
+        {
+          title: "Documentation",
+          icon: "book",
+          path: "https://docs.streamsql.io",
+          external: true,
+        },
+        { title: "Help", icon: "question", path: "/help" },
+      ],
+    },
+  ];
+
   it("hasn't changed", () => {
-    expect(shallow(<App />)).toMatchSnapshot();
+    expect(shallow(<App sections={example_sections} />)).toMatchSnapshot();
   });
 
   describe("Wrapper", () => {
@@ -25,38 +60,20 @@ describe("App", () => {
     });
   });
 
-  const example_sections = [
-    {
-      name: "Resources",
-      items: [
-        { title: "Data Sources", icon: "file-import", path: "/sources" },
-        { title: "Features", icon: "file-code", path: "/features" },
-      ],
-    },
-    {
-      name: "Monitoring",
-      items: [],
-    },
-    {
-      name: "Admin",
-      items: [
-        { title: "Users", icon: "users", path: "/users" },
-        {
-          title: "Documentation",
-          icon: "book",
-          path: "https://docs.streamsql.io",
-          external: true,
-        },
-      ],
-    },
-  ];
-
   describe("parseContentProps", () => {
     it("parses correctly", () => {
       const expected = [
         { title: "Data Sources", path: "/sources" },
+        { title: "Materialized Views", path: "/views" },
         { title: "Features", path: "/features" },
+        { title: "Feature Sets", path: "/feature-sets" },
+        { title: "Training Sets", path: "/training-sets" },
+        { title: "Metrics", path: "/metrics" },
+        { title: "Deployment", path: "/deployment" },
         { title: "Users", path: "/users" },
+        { title: "Settings", path: "/settings" },
+        { title: "Billing", path: "/billing" },
+        { title: "Help", path: "/help" },
       ];
       const actual = parseContentProps(example_sections);
       expect(actual).toEqual(expected);
