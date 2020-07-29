1650c8bbef2a1aa9fbc65dbdf75714b7d8d47e2e
Simba <simba@streamsql.io>
2020-07-29 00:25:02 -0700

    Add ResourceSlice to rootReducers
    This adds ResourceSlice's reducer to the default redux store singleton.

--- dashboard/src/reducers/index.js
+++ dashboard/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import navSectionsReducer from "components/nav/slice";
+import { resourceReducer } from "components/resource-list";
 
 export default combineReducers({
   navSections: navSectionsReducer,
+  resourceList: resourceReducer,
 });
