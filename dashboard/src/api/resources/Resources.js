edfab3482c957e0fae803705c3c2f592a5a63ef0
Simba <simba@streamsql.io>
2020-07-30 15:13:02 -0700

    Move resourceTypes to api/resources
    resourceTypes will be used across different components and lives better
    tied with the API rather than a single component.

---
+++ dashboard/src/api/resources/Resources.js
@@ -0,0 +1,7 @@
+export const resourceTypes = Object.freeze({
+  DATA_SOURCE: "Data Source",
+  MATERIALIZED_VIEW: "Materialized View",
+  FEATURE: "Feature",
+  FEATURE_SET: "Feature Set",
+  TRAINING_SET: "Training Set",
+});
