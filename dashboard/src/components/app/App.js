b389d95f05be50c43107e229cdbe2b1b7e753b95
Simba <simba@streamsql.io>
2020-07-30 15:22:40 -0700

    Update App.js NavSection parser
    The NavSection was extended to include its own view information. This
    allows for the views to be dynamic.
    
    Currently we have two views, Empty which is jsut an empty Box and ResourceList.
    The ResourceAPI is passed into every version, though that should be changed.

--- dashboard/src/components/app/App.js
+++ dashboard/src/components/app/App.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Box from "@material-ui/core/Box";
 import theme from "styles/theme";
 import { ThemeProvider } from "@material-ui/core/styles";
 import Nav from "components/nav";
 import ResourceList from "components/resource-list";
 import { Redirect, Route } from "react-router-dom";
+import ResourcesAPI from "api/resources";
 
 export const App = ({ sections }) => (
   <ThemeWrapper>
     <Nav sections={sections}>{routes(sections)}</Nav>
   </ThemeWrapper>
 );
+
+const apiHandle = new ResourcesAPI();
+
+export const views = {
+  RESOURCE_LIST: "ResourceList",
+  EMPTY: "Empty",
+};
+
+const viewComponent = {
+  [views.RESOURCE_LIST]: ResourceList,
+  [views.EMPTY]: Box,
+};
 
 function routes(sections) {
   const content = parseContentProps(sections);
@@ -21,16 +35,24 @@ function routes(sections) {
 }
 
 function contentRoutes(content) {
-  return content.map((item) => (
-    <Route key={item.path} path={item.path}>
-      <ResourceList title={item.title} />
-    </Route>
-  ));
+  return content.map((item) => {
+    // must be capitalized to work due to:
+    // https://reactjs.org/docs/jsx-in-depth.html#choosing-the-type-at-runtime
+    const Component = viewComponent[item.view];
+    return (
+      <Route key={item.path} path={item.path}>
+        <Component api={apiHandle} {...item.viewProps} />
+      </Route>
+    );
+  });
 }
 
 const defaultRedirect = (content) => {
   const path = indexPath(content);
-  return <Redirect exact key={path} from="/" to={path} />;
+  // If we just use path as the key, it'll collide with the actual Route, so we
+  // prepend default_redirect_.
+  const key = "default_redirect_" + path;
+  return <Redirect exact key={key} from="/" to={path} />;
 };
 
 export const indexPath = (content) => {
@@ -44,8 +66,15 @@ export function parseContentProps(sections) {
   return sections
     .flatMap((section) => section.items)
     .filter((item) => !item.external)
-    .map((item) => ({ title: item.title, path: item.path }));
+    .map(parseItem);
 }
+
+const parseItem = (item) => ({
+  title: item.title,
+  view: item.view || views.EMPTY,
+  viewProps: item.viewProps || {},
+  path: item.path,
+});
 
 export const ThemeWrapper = ({ children }) => (
   <ThemeProvider theme={theme}>
