b513df2b9eabc0842d7f2d12273c514300dbd95c
Simba <simba@streamsql.io>
2020-07-30 15:07:17 -0700

    Add ResourceListView
    ResourceList.js was moved and expanded to only handle logic relevent to
    the view layer.

--- dashboard/src/components/resource-list/ResourceListView.js
+++ dashboard/src/components/resource-list/ResourceListView.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import MaterialTable from "material-table";
@@ -12,21 +13,32 @@ const useStyles = makeStyles((theme) => ({
 
 export const ResourceListView = ({ title, resources, loading, failed }) => {
   const classes = useStyles();
+  const initialLoad = resources == null && !loading;
+  const initRes = resources || [];
+  const copy = (res) => resources.map((o) => ({ ...o }));
+  // MaterialTable can't handle immutable object, we have to make a copy
+  // https://github.com/mbrn/material-table/issues/666
+  const mutableRes = Object.isFrozen(initRes) ? copy(initRes) : initRes;
   return (
-    // MaterialTable doesn't support className, so we wrap it in a Box:
-    // https://github.com/mbrn/material-table/issues/766
     <Box className={classes.table}>
       <MaterialTable
         title={title}
         columns={[{ title: "Name", field: "name" }]}
-        data={resources}
-        isLoading={loading}
+        data={mutableRes}
+        isLoading={initialLoad || loading || failed}
         options={{
           search: true,
         }}
       />
     </Box>
   );
+};
+
+ResourceListView.propTypes = {
+  title: PropTypes.string.isRequired,
+  resources: PropTypes.array,
+  loading: PropTypes.bool,
+  failed: PropTypes.bool,
 };
 
 export default ResourceListView;
