d98b3d66f16f473d62f5100846baa8d07da66b73
Simba <simba@streamsql.io>
2020-07-30 15:07:17 -0700

    Add ResourceListView
    ResourceList.js was moved and expanded to only handle logic relevent to
    the view layer.

--- dashboard/src/components/resource-list/ResourceListView.test.js
+++ dashboard/src/components/resource-list/ResourceListView.test.js
@@ -3,15 +3,55 @@ import React from "react";
 import "jest-canvas-mock";
 import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import produce from "immer";
 
 import { ResourceListView } from "./ResourceListView.js";
 
 configure({ adapter: new Adapter() });
 
 describe("ResourceListView", () => {
-  const sources = shallow(<ResourceListView title={"abc"} />);
+  it("passes through title", () => {
+    const list = shallow(<ResourceListView title="test" title={"test"} />);
+    expect(list.children().props().title).toBe("test");
+  });
 
-  it("passes title to child prop", () => {
-    expect(sources.children().props().title).toEqual("abc");
+  it("sets resources to [] by default", () => {
+    const list = shallow(<ResourceListView title="test" />);
+    expect(list.children().props().data).toEqual([]);
+  });
+
+  it("passes through resources", () => {
+    const list = shallow(
+      <ResourceListView title="test" resources={[{ name: "abc" }]} />
+    );
+    expect(list.children().props().data).toEqual([{ name: "abc" }]);
+  });
+
+  it("makes resources mutable", () => {
+    const immutData = produce([], (draft) => {
+      draft.push(["abc"]);
+    });
+    const list = shallow(
+      <ResourceListView title="test" resources={immutData} />
+    );
+    expect(Object.isFrozen(immutData)).toBe(true);
+    expect(Object.isFrozen(list.children().props().data)).toBe(false);
+  });
+
+  it("sets isLoading when resources isn't set", () => {
+    const list = shallow(<ResourceListView title="test" />);
+    expect(list.children().props().isLoading).toEqual(true);
+  });
+
+  it("sets isLoading when loading", () => {
+    const list = shallow(<ResourceListView title="test" loading={true} />);
+    expect(list.children().props().isLoading).toEqual(true);
+  });
+
+  it("sets isLoading when failed", () => {
+    const list = shallow(
+      <ResourceListView title="test" loading={false} failed={true} />
+    );
+    expect(list.children().props().isLoading).toEqual(true);
   });
 });
