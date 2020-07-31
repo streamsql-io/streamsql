54b3698b2f836a43b15687eeb9d250186e0a6442
Simba <simba@streamsql.io>
2020-07-30 17:12:22 -0700

    Test ResourceList
    We mount the ResourceList in a ReduxWrapper to connect a test store
    to it. We then make sure that fetch is called onComponentMount and
    that the props are being connected properly and passed down to
    ResourceListView.

---
+++ dashboard/src/components/resource-list/ResourceList.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "jest-canvas-mock";
+import ReduxWrapper from "components/redux/wrapper";
+import { configure, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { newTestStore } from "components/redux/store";
+import { resourceTypes } from "api/resources";
+import ResourceList from "./ResourceList.js";
+
+configure({ adapter: new Adapter() });
+
+describe("ResourceList", () => {
+  const dataType = resourceTypes["DATA_SOURCE"];
+  const mockFn = jest.fn(() => wrapInPromise(["abc"]));
+  const mockApi = {
+    fetchResources: mockFn,
+  };
+
+  const component = mount(
+    <ReduxWrapper store={newTestStore()}>
+      <ResourceList api={mockApi} type={dataType} />
+    </ReduxWrapper>
+  );
+
+  it("fetches resources on mount.", () => {
+    expect(mockFn.mock.calls.length).toBe(1);
+    expect(mockFn.mock.calls[0][0]).toEqual(dataType);
+  });
+
+  it("correctly maps inital props from state.", () => {
+    expect(component.find("ResourceListView").props()).toEqual({
+      title: dataType,
+      resources: null,
+      loading: true,
+      failed: false,
+    });
+  });
+});
