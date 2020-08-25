71170b744f50ee4e26a99fe260ac2da2def8db48
Simba <simba@streamsql.io>
2020-08-25 16:26:52 -0700

    Add tags to the resource catalog
    Every resource can include an arbritrary amount of tags. These tags can
    be used to search and filter resources.

--- dashboard/src/api/resources/Resources.js
+++ dashboard/src/api/resources/Resources.js
@@ -10,8 +10,8 @@ export default class ResourcesAPI {
   fetchResources(type) {
     return Promise.resolve({
       data: [
-        { name: "abc", versions: ["abc", "1", "2"] },
-        { name: type, versions: ["1"] },
+        { name: "abc", description: "This is a representation of the wave height", tags: ["Waves", "Surfing"], versions: ["abc", "1", "2"] },
+        { name: type, description: "This embedding captures how advanced a surfer is based on the waves that they surf", versions: ["1"] },
       ],
     });
   }
