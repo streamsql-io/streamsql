844ef3a8dbdc6a9538fa3768e2a2f8851a345b50
Simba <simba@streamsql.io>
2020-08-25 23:30:58 -0700

    Change ResourceListView always copy resources
    There are situations where the outer array isn't frozen but one of
    the inner objects is which breaks MaterialTable. For now, its easier
    to just always copy the resources to guarantee mutability.

--- dashboard/src/components/resource-list/ResourceListView.js
+++ dashboard/src/components/resource-list/ResourceListView.js
@@ -34,7 +34,7 @@ export const ResourceListView = ({
   const copy = (res) => res.map((o) => ({ ...o }));
   // MaterialTable can't handle immutable object, we have to make a copy
   // https://github.com/mbrn/material-table/issues/666
-  const mutableRes = Object.isFrozen(initRes) ? copy(initRes) : initRes;
+  const mutableRes = copy(initRes);
   return (
     <Box className={classes.table}>
       <MaterialTable
