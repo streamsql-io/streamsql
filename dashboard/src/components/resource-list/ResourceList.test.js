2c7bc971d1e3541b3c3119031a32d2a7567f69bf
Simba <simba@streamsql.io>
2020-08-26 01:48:33 -0700

    Test ResourceList tag filtering

--- dashboard/src/components/resource-list/ResourceList.test.js
+++ dashboard/src/components/resource-list/ResourceList.test.js
@@ -5,7 +5,7 @@ import { configure, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { newTestStore } from "components/redux/store";
 import { resourceTypes } from "api/resources";
-import ResourceList from "./ResourceList.js";
+import ResourceList, { makeSelectFilteredResources } from "./ResourceList.js";
 
 configure({ adapter: new Adapter() });
 
@@ -47,5 +47,58 @@ describe("ResourceList", () => {
       "toggleTag",
     ];
     expect(Object.keys(viewProps).sort()).toEqual(expKeys.sort());
+  });
+
+  describe("Resource Filter", () => {
+    it("returns null when resources isn't set", () => {
+      const state = {
+        resourceList: { [dataType]: [] },
+        selectedTags: { [dataType]: {} },
+      };
+      const selector = makeSelectFilteredResources(dataType);
+      expect(selector(state)).toBeNull();
+    });
+
+    it("doesn't filter when no tags are selected", () => {
+      const resList = [{ name: "a", tags: ["1", "2"] }, { name: "b" }];
+      const state = {
+        resourceList: { [dataType]: { resources: resList } },
+        selectedTags: { [dataType]: {} },
+      };
+      const selector = makeSelectFilteredResources(dataType);
+      expect(selector(state)).toEqual(resList);
+    });
+
+    it("filters using tag", () => {
+      const resList = [
+        { name: "a", tags: ["1", "2"] },
+        { name: "b" },
+        { name: "c", tags: ["1"] },
+        { name: "d", tags: ["2"] },
+      ];
+      const state = {
+        resourceList: { [dataType]: { resources: resList } },
+        selectedTags: { [dataType]: { "1": true } },
+      };
+      const selector = makeSelectFilteredResources(dataType);
+      const expected = [0, 2].map((idx) => resList[idx]);
+      expect(selector(state)).toEqual(expected);
+    });
+
+    it("filters using multiple tags", () => {
+      const resList = [
+        { name: "a", tags: ["1", "2"] },
+        { name: "b" },
+        { name: "c", tags: ["1"] },
+        { name: "d", tags: ["2"] },
+      ];
+      const state = {
+        resourceList: { [dataType]: { resources: resList } },
+        selectedTags: { [dataType]: { "1": true, "2": true } },
+      };
+      const selector = makeSelectFilteredResources(dataType);
+      const expected = [0].map((idx) => resList[idx]);
+      expect(selector(state)).toEqual(expected);
+    });
   });
 });
